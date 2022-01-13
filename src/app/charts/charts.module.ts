9a5e6fc6486415f2334778cf42d64deb6ff15975
EmanuelTejada032 <emanueltejada31295@gmail.com>
2022-01-13 08:40:10 -0400

    Add basic directory structure

--- src/app/charts/charts.module.ts
+++ src/app/charts/charts.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChartsRoutingModule } from './charts-routing.module';
+import { BarsComponent } from './pages/bars/bars.component';
+import { DoubleBarsComponent } from './pages/double-bars/double-bars.component';
+import { CircularComponent } from './pages/circular/circular.component';
+import { ChartBarComponent } from './components/chart-bar/chart-bar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BarsComponent,
+    DoubleBarsComponent,
+    CircularComponent,
+    ChartBarComponent
+  ],
   imports: [
     CommonModule,
     ChartsRoutingModule
